1251f5823495f782f3118439e6d1302bce9b6410
Prathibha <95700454+Prathibha-S@users.noreply.github.com>
2022-08-22 15:32:57 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,14 +1,38 @@
-package com.example.displayhello;
+package com.example.loginform;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1,b2;
+    EditText e1,e2;
+    TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        e1=(EditText)findViewById(R.id.editText1);
+        e2=(EditText)findViewById(R.id.editText2);
+        t1=(TextView)findViewById(R.id.textView6);
+        b1=(Button)findViewById(R.id.button);
+        b2=(Button)findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                e1.setText("");
+                e2.setText(" ");
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                t1.setText("Username is:"+e1.getText()+"\nPassword is:"+e2.getText());
+            }
+        });
     }
+
 }
