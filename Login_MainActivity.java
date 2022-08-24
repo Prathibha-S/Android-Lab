3a1aa617fa6372170f8a11d279bd2a115d4e4400
Prathibha <95700454+Prathibha-S@users.noreply.github.com>
2022-08-24 14:42:08 +0530

    Add files via upload

---
+++ Login_MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.program3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    EditText t1,t2;
+    Button b1,b2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        t1=(EditText)findViewById(R.id.editText1);
+        t2=(EditText)findViewById(R.id.editText2);
+        b1=(Button)findViewById(R.id.button);
+        b2=(Button)findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (t1.getText().toString().equals("admin") &&
+                        t2.getText().toString().equals("admin")) {
+                    Toast.makeText(getApplicationContext(), "Login Successfull", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Login Unsuccessfull", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                t1.setText((" "));
+                t2.setText(" ");
+            }
+        });
+    }
+}
