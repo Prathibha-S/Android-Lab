555211d49aa6b936606c54a7f318a625a4182d5a
Prathibha <95700454+Prathibha-S@users.noreply.github.com>
2022-11-02 14:33:53 +0530

    Add files via upload

---
+++ OptionsMenu/MainActivity/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_file, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(MainActivity.this, "Item_1 Selected", Toast.LENGTH_SHORT).show();
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.item2:
+                Toast.makeText(MainActivity.this, "Item_2 Selected", Toast.LENGTH_SHORT).show();
+                Intent i2 = new Intent(getApplicationContext(), SecondP.class);
+                startActivity(i2);
+                return true;
+            case R.id.item3:
+                Toast.makeText(MainActivity.this, "Item_3 Selected", Toast.LENGTH_SHORT).show();
+                Intent i3 = new Intent(getApplicationContext(), ThirdP.class);
+                startActivity(i3);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
---
+++ OptionsMenu/MainActivity/ThirdP.java
@@ -0,0 +1,49 @@
+package com.example.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class ThirdP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_third_p);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_file, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(getApplicationContext(), "Item_1 Selected", Toast.LENGTH_SHORT).show();
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+                return true;
+            case R.id.item2:
+                Toast.makeText(getApplicationContext(), "Item_2 Selected", Toast.LENGTH_SHORT).show();
+                Intent i2=new Intent(getApplicationContext(),SecondP.class);
+                startActivity(i2);
+                return true;
+            case R.id.item3:
+                Toast.makeText(getApplicationContext(), "Item_3 Selected", Toast.LENGTH_SHORT).show();
+                Intent i3=new Intent(getApplicationContext(),ThirdP.class);
+                startActivity(i3);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
